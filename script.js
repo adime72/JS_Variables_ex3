d306bc9108f02fb1466e002b17c3daf59f15fdbb
Adi Meirovitch <adime.support@gmail.com>
2017-10-30 14:01:17 +0200

    2 array's var's numbers & objects

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+// Creating an array with values
+var nmnrs = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];  
+// Creating an Object with data
+
+console.log(nmbs.length);
+
+
+//task 7 in ex3
+var studentsme =[{
+  eyes: "blue",
+  name: "Adi",
+  hight: 176,
+
+  },]
+
+console.log(JSON.stringify(me));
+
