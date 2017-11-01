3e1b21447fdefc773373f59c042d87f1bb9c6267
Adi Meirovitch <adime.support@gmail.com>
2017-11-01 02:25:24 +0200

    the all project

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 
 
 // Creating an array with values
-var nmnrs = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];  
-// Creating an Object with data
+var nmbrs = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];  
 
-console.log(nmbs.length);
+
+console.log(nmbrs.length);
+console.log(nmbrs[0,2,5]); 
+
+
+nmbrs.splice(8, 2);  // will remove the 2 last items
+console.log(nmbrs); 
+
+var mixAlefnum = [1, "אחת", 2, "שתיים", 3, "שלוש", 5, "חמש", 8, "שמונה"]; 
+console.log(mixAlefnum); 
 
 
 //task 7 in ex3
@@ -13,8 +21,48 @@ var studentsme =[{
   eyes: "blue",
   name: "Adi",
   hight: 176,
+  }, 
+  {
+    eyes: "brown",
+    name: "Noam",
+    hight: 178,
+    },
+    {
+      eyes: "blue",
+      name: "Marina",
+      hight: 168,
+      },
+      {
+        eyes: "green",
+        name: "Hen",
+        hight: 165,
+        },
+        {
+          eyes: "brown",
+          name: "Alex",
+          hight: 173,
+          },
+          {
+            eyes: "black",
+            name: "Michal",
+            hight: 165,
+            }
+];
 
-  },]
+console.log(JSON.stringify(studentsme));
 
-console.log(JSON.stringify(me));
-
+studentsme.shift();
+console.log(studentsme);
+studentsme.unshift({
+  eyes: "brown",
+  name: "Nir",
+  hight: 173,
+  });       // Adding to the start
+console.log(studentsme);
+//The switch
+studentsme.splice(0, 1);  // will remove the first item
+//Adding the new object
+var face = {
+  eyes: "greenish"};
+  studentsme.unshift(face);       // Adding to the start
+  console.log(studentsme); 
